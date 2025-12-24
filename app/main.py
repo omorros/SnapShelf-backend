50eac9ea89afb55505c8d78055efacd933eee3ee
Oriol Morros Vilaseca <OM368@student.aru.ac.uk>
2025-12-24 19:25:54 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,9 @@
 from fastapi import FastAPI
+from sqlalchemy import text
+from fastapi import Depends
+from sqlalchemy.orm import Session
+from app.core.database import get_db
+
 
 app = FastAPI(
     title="SnapShelf Backend",
@@ -8,3 +13,8 @@ app = FastAPI(
 @app.get("/health")
 def health_check():
     return {"status": "ok"}
+
+@app.get("/db-test")
+def db_test(db: Session = Depends(get_db)):
+    result = db.execute(text("SELECT 1")).scalar()
+    return {"db_response": result}
